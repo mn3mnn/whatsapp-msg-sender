eb49a07558130653ab3c4311455c6a8848dc53b6
Abdelmonem Tarek <11410120210220@stud.cu.edu.eg>
2024-01-29 20:17:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,8 @@ manager = get_manager()
 
 
 def main():
-    args = sys.argv
-
-    n_accounts = 1
-    if len(args) > 1:
-        try:
-            n_accounts = int(args[1])
-        except Exception as e:
-            pass
-
-    timeout = None
-    if len(args) > 2:
-        try:
-            timeout = int(args[2])
-        except Exception as e:
-            pass
-
+    timeout = int(input("enter waiting timeout for each message status (in seconds)"))
+    n_accounts = int(input("enter num of accounts to add:"))
     for i in range(n_accounts):
         try:
             mobile_number = str(input(f"Enter mobile number of account {i+1}: "))
